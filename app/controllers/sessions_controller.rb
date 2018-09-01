a9a4b39c82f7091737b5e586a55737ac8b5cb8bf
Yina Zhu <yinagit@gmail.com>
2018-09-01 23:15:59 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+    return redirect_to login_path if params[:name].blank?
     session[:username] = params[:username]
     redirect_to '/'
   end
