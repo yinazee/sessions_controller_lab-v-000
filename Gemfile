e5f5165b6f8d986fef0c26d63597e5f57d481e51
github username <github email address>
2018-09-02 00:55:51 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ group :development do
 end
 
 group :development, :test do
+  gem 'pry'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    return redirect_to login_path if params[:name].blank?
-    session[:username] = params[:username]
-    redirect_to '/'
+      return redirect_to(controller: 'sessions',
+                         action: 'new') if !params[:name] || params[:name].empty?
+      session[:name] = params[:name]
+      redirect_to controller: 'application', action: 'hello'
+    end
+
+  def destroy
+    session.delete :name
+    redirect_to controller: 'application', action: 'hello'
   end
 end
