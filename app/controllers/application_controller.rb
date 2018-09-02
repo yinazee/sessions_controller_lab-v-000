5eb15530ae279e6b1edc6252d5c6fae2dc6683f6
github username <github email address>
2018-09-02 01:08:03 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    redirect_to controller :'sessions', action: 'new' unless session[:name]
+    redirect_to controller:'sessions', action: 'new' unless session[:name]
   end
 end
